cf18ac0d5c4726b79937ca05cf919df4a6718624
Quanbui86 <136612066+Quanbui86@users.noreply.github.com>
2023-12-12 17:34:37 +0700

    Big update

--- script.js
+++ script.js
@@ -11,50 +11,65 @@ const inputUN = document.querySelector('[id="username"]');
 const inputPass = document.querySelector('[id="password"]');
 const inputBirth = document.getElementById('birth')
 
+patternUserName = /(^[a-zA-Z0-9]{4,20}$)/;
+patternPass = /^.{4,16}$/;
+patternName = /^[a-zA-Z\x20]{4,20}$/;
 function handleFocusUName(){
 }
 // Xử lí username Label khi focus
 inputUN.addEventListener('focus', function() {
-    unameLabel.style.transition = 'opacity 1s linear 0.3s'
     unameLabel.style.opacity = '1';
+    unameLabel.style.color = 'green';
+    unameLabel.style.backgroundColor = 'white';
+    //unameLabel.style.transition = 'all 1s linear 0.3s'
 });
 // Xử lí username Label khi không focus vào input hoặc input username có giá trị
 
 inputUN.addEventListener('blur', function() {
-    if (inputUN.value){ 
-        unameLabel.style.transition = 'opacity 0.5s linear'
+    if (patternUserName.test(inputUN.value)){ 
+        //unameLabel.style.transition = 'opacity 0.5s linear'
         unameLabel.style.opacity = '1';
+        document.getElementsByClassName('errorMsg')[0].style.opacity = '0'
     }else {
         unameLabel.style.opacity = '0';
-        unameLabel.style.transition = 'opacity 0.5s linear'}
+        //unameLabel.style.transition = 'opacity 0.5s linear'
+        document.getElementsByClassName('errorMsg')[0].style.opacity = '1'
+    }
 });
 
 inputPass.addEventListener('focus', function(){
     passLabel.style.opacity = '1';
-    passLabel.style.transition = 'opacity 1s linear 0.3s'
+    passLabel.style.color = 'green';
+    passLabel.style.backgroundColor = 'white';
+    //passLabel.style.transition = 'opacity 1s linear 0.3s'
 });
 inputPass.addEventListener('blur', function(){
-    if(inputPass.value){
+    if(patternPass.test(inputPass.value)){
         passLabel.style.opacity = '1';
-        passLabel.style.transition = 'opacity 1s linear'
+        //passLabel.style.transition = 'opacity 1s linear';
+        document.getElementsByClassName('errorMsg')[1].style.opacity = '0'
     }else {
         passLabel.style.opacity = '0';
-        passLabel.style.transition = "opacity 0.5s linear"
-    }
-    
+       // passLabel.style.transition = "opacity 0.5s linear";
+        document.getElementsByClassName('errorMsg')[1].style.opacity = '1'
+    }    
 });
 
 inputName.addEventListener('focus', function(){
     nameLabel.style.opacity = '1';
-    nameLabel.style.transition = 'opacity 1s linear 0.3s';
+    nameLabel.style.color = 'green';
+    nameLabel.style.backgroundColor = 'white';
+    //nameLabel.style.transition = 'opacity 1s linear 0.3s';
 })
 inputName.addEventListener('blur', function(){
-    if(inputName.value){
+    if(patternName.test(inputName.value)){
         nameLabel.style.opacity = '1';
-        nameLabel.style.transition = 'opacity 1s linear'
+        //nameLabel.style.transition = 'opacity 1s linear';
+        document.getElementsByClassName('errorMsg')[2].style.opacity = '0';
     }else {
         nameLabel.style.opacity = '0';
-        nameLabel.style.transition = "opacity 0.5s linear"
+        //nameLabel.style.transition = "opacity 0.5s linear";
+        document.getElementsByClassName('errorMsg')[2].style.opacity = '1'
     }
     
 });
@@ -62,17 +77,22 @@ inputName.addEventListener('blur', function(){
 // Xử lí birth Label khi focus vào input birth
 inputBirth.addEventListener('focus', function() {
     birthLabel.style.opacity = '1';
-    birthLabel.style.transition = 'opacity 1s linear 0.3s'
+    birthLabel.style.color = 'green';
+    birthLabel.style.backgroundColor = 'white';
+    //birthLabel.style.transition = 'opacity 1s linear 0.3s'
 });
 // Xử lí birth Label khi input birth có giá trị
 
 inputBirth.addEventListener('blur', function() {
-    if (inputBirth.value){ 
+    if (Boolean(inputBirth.value)){ 
         birthLabel.style.opacity = '1';
-        birthLabel.style.transition = 'opacity 0.5s linear'
+        //birthLabel.style.transition = 'opacity 0.5s linear'
+        document.getElementsByClassName('errorMsg')[3].style.opacity = '0'
     }else {
         birthLabel.style.opacity = '0';
-        birthLabel.style.transition = 'opacity 0.5s linear'}
+        //birthLabel.style.transition = 'opacity 0.5s linear'};
+        document.getElementsByClassName('errorMsg')[3].style.opacity = '1'
+    }
 });
 
 // Hiệu ứng khi fade-in sử dụng DOMContentLoaded, chạy trước khi file HTML load xong
